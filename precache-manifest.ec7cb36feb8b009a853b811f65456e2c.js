b2bb7eda5af370f8521bce0968966a88319933bb
Nick Ciambrone <nickciambrone@Nicks-MacBook-Pro.local>
2020-07-13 23:44:07 -0400

    Updates

---
+++ precache-manifest.ec7cb36feb8b009a853b811f65456e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd06c02f2a6d1327f4cd65e8f93b67a",
+    "url": "/insight-bets/index.html"
+  },
+  {
+    "revision": "9bcb5b948e65d8862ddd",
+    "url": "/insight-bets/static/css/main.901ce9b6.chunk.css"
+  },
+  {
+    "revision": "490a4468971ab75a7d65",
+    "url": "/insight-bets/static/js/2.a60bfb3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/insight-bets/static/js/2.a60bfb3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bcb5b948e65d8862ddd",
+    "url": "/insight-bets/static/js/main.a3b8e296.chunk.js"
+  },
+  {
+    "revision": "9d3af43f9714a610eb08",
+    "url": "/insight-bets/static/js/runtime-main.4c77c28b.js"
+  }
+]);
