3e1a1a7c95c2c75863c8f9cca8d8fd6bfa5d1cfb
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2020-07-27 15:19:14 -0400

    finalizing

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import CheckOutPage from './pages/checkout/checkout.component'
+import ContactPage from './pages/contact/contact.component'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
 import {selectCurrentUser} from './redux/user/user.selectors.js'
 import { createStructuredSelector } from 'reselect';
+
 class App extends React.Component {
 
   unsubscribeFromAuth = null;
@@ -59,7 +61,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckOutPage} />
-
+          <Route exact path = '/contact' component = {ContactPage} />
           <Route
             exact
             path='/sign-in'
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './contact.styles.scss'
+const ContactPage = () =>(
+    <div className = 'contact-page'>
+
+        Name: <input type = 'text'></input> <br />
+        email: <input type = 'text'></input> <br />
+        Message: <input type = 'text'></input>
+
+    </div>
+)
+export default ContactPage;
